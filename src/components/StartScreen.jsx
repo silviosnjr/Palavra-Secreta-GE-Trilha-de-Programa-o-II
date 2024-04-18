6d3f7e2efb80820b04ee09546e92f33882b00f71
Monique Caldeira <nique.sca@gmail.com>
2024-04-18 10:44:31 -0300

    criação dos componentes game, gameover e startscreen. Desenvolvimento da logica do jogo

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,11 +1,11 @@
 import './StartScreen.css'
 
-const StartScreen = () => {
+const StartScreen = ({startGame}) => {
   return (
     <div className='start'>
       <h1>Screte Word</h1>
       <p>Clique no botão abaixo para começar a jogar!</p>
-      <button>Começar o Jogo</button>
+      <button onClick={startGame}>Começar o Jogo</button>
     </div>
   )
 }
