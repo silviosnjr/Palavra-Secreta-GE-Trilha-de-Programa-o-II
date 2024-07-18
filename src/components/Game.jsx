c056d40b7ef1f44bc887d376e99eec0075fb8317
Monique Caldeira <nique.sca@gmail.com>
2024-07-18 10:28:36 -0300

    melhorias na interface

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -31,14 +31,14 @@ const Game = ({
 
   return (
     <div className="game">
-      <h2>Tempo restante: {time}s</h2>
+      <p>Tempo restante: {time}s</p>
       <p className="points"></p>
       <span>Pontuação: {score}</span>
-      <h1>Adivinhe a palavra:</h1>
+      <h2>Adivinhe a palavra:</h2>
       <h3 className="tip">
         Dica sobre a palavra: <span>{pickedCategory}</span>
       </h3>
-      <h3>Você ainda tem {guesses} tentativas</h3>
+      <p>Você ainda tem {guesses} tentativas</p>
       <div className="wordContainer">
         {letters.map((letter, i) =>
           guessedLetters.includes(letter) ? (
