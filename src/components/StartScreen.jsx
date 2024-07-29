f60a9c44e00e6cb1e8a9379b77806ee494c641ae
Silvio Sales do Nascimento Junior <silviosnjr@gmail.com>
2024-07-29 12:05:03 -0300

    teste

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -3,7 +3,7 @@ import './StartScreen.css'
 const StartScreen = ({startGame}) => {
   return (
     <div className='start'>
-      <h1>Roda Roda Start</h1>
+      <h1>Roda Roda GE Trilha de Programação II</h1>
       <p>Clique no botão abaixo para começar a jogar!</p>
       <button onClick={startGame}>Começar o Jogo</button>
     </div>
