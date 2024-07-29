6c6b1eb531ed1c9426aa88560c0ffff9d3355911
Silvio Sales do Nascimento Junior <silviosnjr@gmail.com>
2024-07-29 11:45:46 -0300

    novas palavras

--- src/data/words.js
+++ src/data/words.js
@@ -1,4 +1,5 @@
 export const wordsList = {
-   palavras chaves do JavaScript: ["function", "const", "let", "if", "else", "for"],
-   elementos do HTML: ["div", "h1", "button", "body", "head", "title"]
+  javascript: ["function", "const", "let", "if", "else", "for"],
+  html: ["div", "h1", "button", "body", "head", "title"],
+  css: ["style", "padding", "color", "display", "flex", "font-size"]
  };
