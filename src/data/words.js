7d6868e57fcb9ccead431e6a999a6487a297163d
Silvio Sales do Nascimento Junior <silviosnjr@gmail.com>
2024-07-29 11:41:40 -0300

    novas palavras

--- src/data/words.js
+++ src/data/words.js
@@ -1,9 +1,4 @@
 export const wordsList = {
-   escola: ["Caderno", "Lapis", "Professor", "Aluno", "Quadro", "Livro", "Lousa", "Carteira", "Mochila", "Biblioteca"],
-   fruta: ["Banana", "Laranja", "Abacaxi", "Morango", "Uva", "Melancia", "Kiwi"],
-   educacao: ["Ensino", "Aprendizagem", "Didatica", "Pedagogia", "Curriculo", "Docente", "Discente", "Metodologia"],
-   computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Processador", "Memoria", "Fonte"],
-   programacao: ["Linguagem", "JavaScript", "Scratch", "Variavel", "Funçao", "Compilador", "Bug", "Algoritmo", "Biblioteca", "Lista", "Loop", "Condicional", "Recursao"],
-   alimento: ["Arroz", "Feijao", "Leite", "Queijo", "Manteiga"],
-   animais: ["Cachorro", "Gato", "Elefante", "Tigre", "Girafa", "Pato", "Coelho", "Cavalo"],
+   palavras chaves do JavaScript: ["function", "const", "let", "if", "else", "for"],
+   elementos do HTML: ["div", "h1", "button", "body", "head", "title"]
  };
